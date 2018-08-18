11c1c2aac336931e89b8eead300cb8f4adfba430
MOATEZ AMAIRI <35851660+M-AMAIRI@users.noreply.github.com>
2018-08-18 19:13:06 +0200

    Create mask
    Create mask

--- create_MASK.cpp
+++ create_MASK.cpp
@@ -1,16 +1,4 @@
 // create_MASK.cpp : Defines the entry point for the console application.
-//
-
-/*
-
-In this tutorial you see a simple method to apply a binary mask to an RGB image :
-step 1 : select a region and create mask by using any Software , like Paint on windows .the height and width of the original image and the mask should be the same
-step 2 : Load an image using imread
-step 3 : Transform an image from BGR to Grayscale format by using cvtColor
-step 4 : Save your transformed image in a file on disk 
-
-*/
-
 
 #include "stdafx.h"
 #include "opencv2/opencv.hpp"
